63533e01c5a763aecd31f780fa6d4b9acdb0d5fb
Yash <yashwanth.venkatesan@gmail.com>
2025-05-19 01:57:43 -0400

    fixed animation logic

--- src/Section1.tsx
+++ src/Section1.tsx
@@ -2,12 +2,135 @@ import { logoData } from "./logo";
 
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
+import Lenis from "lenis";
+import { useEffect, useRef } from "react";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Section1 = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const lenis = new Lenis();
+
+  lenis.on("scroll", ScrollTrigger.update);
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+
+  gsap.ticker.lagSmoothing(0);
+
+  const initialOverlayScale = 350;
+
+  const overlayCopy = document.getElementById("overlay-copy");
+
+  useEffect(() => {
+    const logoContainer = document.getElementById("logo-container");
+    const logoMask = document.getElementById(
+      "logoMask"
+    ) as SVGPathElement | null;
+    if (!logoContainer || !logoMask) return;
+
+    logoMask.setAttribute("d", logoData);
+    const logoDimensions = logoContainer.getBoundingClientRect();
+    const logoBoundingBox = logoMask.getBBox();
+
+    const horizontalScaleRatio = logoDimensions.width / logoBoundingBox.width;
+    const verticalScaleRatio = logoDimensions.height / logoBoundingBox.height;
+
+    const logoScaleFactor = Math.min(horizontalScaleRatio, verticalScaleRatio);
+
+    const logoHorizontalPosition =
+      logoDimensions.left +
+      (logoDimensions.width - logoBoundingBox.width * logoScaleFactor) / 2 -
+      logoBoundingBox.x * logoScaleFactor;
+
+    const logoVerticalPosition =
+      logoDimensions.top +
+      (logoDimensions.height - logoBoundingBox.height * logoScaleFactor) / 2 -
+      logoBoundingBox.y * logoScaleFactor;
+
+    logoMask.setAttribute(
+      "transform",
+      `translate(${logoHorizontalPosition}, ${logoVerticalPosition}) scale(${logoScaleFactor})`
+    );
+
+    gsap.set(logoContainer, {
+      scale: logoScaleFactor,
+      transformOrigin: "center",
+      y: logoVerticalPosition,
+    });
+  }, []);
+  ScrollTrigger.create({
+    trigger: "#hero",
+    start: "top top",
+    end: `${window.innerHeight * 5}px`,
+    pin: true,
+    pinSpacing: true,
+    scrub: 1,
+    onUpdate: (self) => {
+      if (!overlayCopy) return;
+      const scrollProgress = self.progress;
+
+      const fadeOpacity = 1 - scrollProgress * (1 / 0.15);
+
+      if (scrollProgress <= 0.15) {
+        gsap.set(["#hero-img-copy", "#hero-img-logo"], {
+          opacity: fadeOpacity,
+        });
+      } else {
+        gsap.set(["#hero-img-copy", "#hero-img-logo"], {
+          opacity: 0,
+        });
+      }
+
+      if (scrollProgress <= 0.85) {
+        const normalizedProgress = scrollProgress * (1 / 0.85);
+        const heroImgContinerScale = 1.5 - 0.5 * normalizedProgress;
+        const overlayScale =
+          initialOverlayScale *
+          Math.pow(1 / initialOverlayScale, normalizedProgress);
+
+        gsap.set("#hero-img-container", {
+          scale: heroImgContinerScale,
+        });
+        gsap.set("#overlay", {
+          scale: overlayScale,
+        });
+      }
+
+      const fadeOverlayOpacity =
+        scrollProgress >= 0.25
+          ? Math.min(1, scrollProgress - 0.25) * (1 / 0.4)
+          : 0;
+      gsap.set("#fade-overlay", {
+        opacity: fadeOverlayOpacity,
+      });
+
+      if (scrollProgress >= 0.6 && scrollProgress <= 0.85) {
+        const overlayCopyRevealProgress = (scrollProgress - 0.6) * (1 / 0.25);
+
+        const gradienSpread = 100;
+        const gradientBottomPosition = 140 - overlayCopyRevealProgress * 280;
+        const gradientTopPosition = gradientBottomPosition - gradienSpread;
+        const overlayCopyScale = 1.25 - 0.25 * overlayCopyRevealProgress;
+
+        overlayCopy.style.background = `linear-gradient(to bottom, 
+        #111117 0%, #111117 ${gradientTopPosition}%, #e66461 ${gradientBottomPosition}%, #e66461 100%)`;
+        overlayCopy.style.backgroundClip = "text";
+
+        gsap.set(overlayCopy, {
+          scale: overlayCopyScale,
+          opacity: overlayCopyRevealProgress,
+        });
+      } else if (scrollProgress < 0.6) {
+        gsap.set(overlayCopy, {
+          opacity: 0,
+        });
+      }
+    },
+  });
+
   return (
-    <section>
+    <section ref={containerRef} id="hero">
       <div id="hero-img-container">
         <img src="/bg.webp" alt="" />
 
